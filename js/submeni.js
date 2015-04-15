9419264187a99552adc8b68d741617b071041955
Anesa Drakovac <adrakovac1@etf.unsa.ba>
2015-04-15 02:43:44 +0200

    WT - Second spiral
    Submeni

---
+++ js/submeni.js
@@ -0,0 +1,8 @@
+function galerija_Prikazi(){
+   document.getElementById("submeni").style.display="block";
+   document.getElementById("submeni").style.clear="both";
+}
+ 
+function galerija_Sakrij(){
+	document.getElementById("submeni").style.display="none";
+}
