f4f241ddfefc13e2e8ccbf8af0ea2b657a262ce9
Anesa Drakovac <adrakovac1@etf.unsa.ba>
2015-05-02 15:54:09 +0200

    Ucitavanje podstranica AJAX | Tabela proizvoda

---
+++ js/ucitajStranicuAsinhrono.js
@@ -0,0 +1,14 @@
+function ucitajStranicuAsinhrono(url) {
+    var ajax = new XMLHttpRequest;
+
+    ajax.onreadystatechange = function() {
+        if(ajax.status == 200 && ajax.readyState == 4) {
+            document.open();
+            document.write(ajax.responseText);
+            document.close();
+        }
+    }
+
+    ajax.open("GET", url, true);
+    ajax.send();
+}
