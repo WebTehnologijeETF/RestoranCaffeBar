0960955af6837c6ec654ff561617c83a463bdd34
Anesa Drakovac <adrakovac1@etf.unsa.ba>
2015-05-04 23:03:03 +0200

    Validacija putem AJAXa
    Validacija: Općina i srednja škola

--- js/kontakt.js
+++ js/kontakt.js
@@ -30,7 +30,7 @@ function validacijaMail() {
         document.getElementById("errorMail").innerHTML="Morate unijeti mail!";
         valid = false;
     }
-    else if (!mail.value.match(/^[a-z0-9_]+@[a-z]+\.[a-z][a-z]+$/)) {
+    else if (!mail.value.match(/^[a-z0-9_]+@[a-z.]+\.[a-z][a-z]+$/)) {
         mail.style.backgroundColor="red";
         document.getElementById("errorMail").style.display="block";
         document.getElementById("errorMail").innerHTML="Ispravno unesite mail!";
@@ -84,6 +84,8 @@ $(document).ready(function(){
         validacijaMail();
         validacijaPoruka();
 		validacijaTelefon();
+		validacijaOpcina();
+		validacijaSkola();
 
         var posalji = document.getElementById("posalji");
         if (true == valid) {
@@ -91,3 +93,60 @@ $(document).ready(function(){
         }		
     });
 });
+
+function validacijaOpcina() {
+	var opcina = document.getElementById("opcina");
+
+    if (opcina.value.length != 0 && !opcina.value.match(/^[a-zA-Z ČčĆćŽžŠšĐđ]+$/)) {
+        opcina.style.backgroundColor="red";
+        document.getElementById("errorOpcina").style.display="block";
+        document.getElementById("errorOpcina").innerHTML="Ispravno unesite naziv općine!";
+        valid = false;
+    }
+    else {
+        mail.style.backgroundColor="#faf3ed";
+        document.getElementById("errorTelefon").style.display="none";
+    }
+}
+
+function validacijaSkola() {
+    var skola = document.getElementById("skola");
+    var opcina = document.getElementById("opcina");
+
+    var ajax = new XMLHttpRequest;
+
+    ajax.onreadystatechange = function() {
+        if(ajax.status == 200 && ajax.readyState == 4) {
+            var odgovor = JSON.parse(ajax.responseText);
+
+            var pogodak = false;
+            if (Object.keys(odgovor)[0] == "ok")
+                pogodak = true;
+
+            if (false == pogodak && opcina.value.length > 0 && skola.value.length > 0) {
+                skola.style.backgroundColor="red";
+                document.getElementById("errorSkola").style.display="block";
+                document.getElementById("errorSkola").innerHTML=odgovor.greska;
+                valid = false;
+            }
+            else {
+                skola.style.backgroundColor="#faf3ed";
+                document.getElementById("errorSkola").style.display="none";
+            }
+        }
+    }
+
+    ajax.open("GET", "http://zamger.etf.unsa.ba/wt/srednja_skola.php?opcina=" + opcina.value + "&skola=" + skola.value, true);
+    ajax.send();
+
+    if (skola.value.length != 0 && !skola.value.match(/^[a-zA-Z ČčĆćŽžŠšĐđ]+$/)) {
+        skola.style.backgroundColor="red";
+        document.getElementById("errorSkola").style.display="block";
+        document.getElementById("errorSkola").innerHTML="Ispravno unesite školu!";
+        valid = false;
+    }
+    else {
+        skola.style.backgroundColor="#faf3ed";
+        document.getElementById("errorSkola").style.display="none";
+    }
+}
